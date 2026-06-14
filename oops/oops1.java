2950f0dac47d24f82e4c2e90ec7eb3e1d11a14ba
prashant thainuan <prashantthainuan38@gmail.com>
2026-06-14 14:48:19 +0530

    first opps

---
+++ oops/oops1.java
@@ -0,0 +1,13 @@
+class Student{
+    String name;
+    int age;
+    void study(){
+        System.out.println("study");
+    }
+}
+public class oops1 {
+    public static void main(String[] args) {
+         
+    }
+    
+}
