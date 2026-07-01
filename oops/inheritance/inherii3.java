2820d377ebc511d2fb26476623d485823c297ee9
prashant thainuan <prashantthainuan38@gmail.com>
2026-07-01 13:47:44 +0530

    ruels for overridding

---
+++ oops/inheritance/inherii3.java
@@ -0,0 +1,33 @@
+package inheritance;
+class State{
+
+}
+class City{
+
+}
+//  methid overridden 
+class Animal{
+ public void eat(){
+    System.out.println("eat anything ");
+ }
+ public State n(){
+    System.out.println("name");
+    State s = new State();
+    return s;
+;
+ }
+}
+class Tiger extends Animal   {
+ public void eat(){
+    System.out.println("EAT FLESh ");
+ }
+ public city n (){
+    City c = new City();
+    return c ;// this
+ }
+}
+public class inherii3 {
+    public static void main(String[] args) {
+         
+    }
+}
