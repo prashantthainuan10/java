927558a79abc3bb7061a1596eea3ad125088fb59
prashant thainuan <prashantthainuan38@gmail.com>
2026-07-30 10:23:10 +0530

    selection sort

--- datastructures/selectionsort.java
+++ datastructures/selectionsort.java
@@ -2,28 +2,29 @@
 
 public class selectionsort {
     public static void main(String[] args) {
-        int[] nums={46,2,6,5,1};
-        int size=nums.length;
-        int temp=0;
-        int min=0;
-        for(int i=0;i<size-1;i++ ){
-            for(int j =i;j<size;j++){
-                 min = nums[j];
-                if(nums[j]<nums[j+1]){
-                    min=nums[j];
-                }
-                else{
-                    min=nums[j+1];
+
+        int[] nums = {46, 2, 6, 5, 1};
+        int size = nums.length;
+
+        for (int i = 0; i < size - 1; i++) {
+
+            int minIndex = i;
+
+            for (int j = i + 1; j < size; j++) {
+                if (nums[j] < nums[minIndex]) {
+                    minIndex = j;
                 }
             }
-            temp=nums[i];
-            nums[i]=min;
-            min=temp;
+
+            int temp = nums[i];
+            nums[i] = nums[minIndex];
+            nums[minIndex] = temp;
         }
-        System.out.println();
-        System.out.println("sorted aray");
-        for(int ele:nums){
-            System.out.println(ele +"");
+
+        System.out.println("Sorted Array");
+
+        for (int ele : nums) {
+            System.out.print(ele + " ");
         }
     }
 }
